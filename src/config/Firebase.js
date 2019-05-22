d00c302edf0b1c539cca8c1780998d03caa1add6
fatsay <fatih_sayilir@hotmail.com>
2019-05-22 13:52:10 +0200

    Update Firebase.js

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 // Initialize Firebase
 const config = {
-        apiKey: "AIzaSyDEiC8s89U5JgIJykVEw3et-0biVtSx1rk",
-        authDomain: "fir-test-bcbfe.firebaseapp.com",
-        databaseURL: "https://fir-test-bcbfe.firebaseio.com",
-        projectId: "fir-test-bcbfe",
-        storageBucket: "fir-test-bcbfe.appspot.com",
-        messagingSenderId: "399020136811"
+        
 };
 
 firebase.initializeApp(config);
